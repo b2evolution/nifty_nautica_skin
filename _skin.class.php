ec529fb241ca5f8a5457ba016dd563efa5ac3741
Milos Zivojinovic <miloszivojinovic93@gmail.com>
2016-11-02 11:21:44 +0100

    New skinshot

--- _skin.class.php
+++ _skin.class.php
@@ -17,7 +17,7 @@ if( !defined('EVO_MAIN_INIT') ) die( 'Please, do not access this page directly.'
  */
 class nifty_nautica_Skin extends Skin
 {
-	var $version = '1.1';
+	var $version = '1.1.1';
 
 	/**
 	 * Get default name for the skin.
